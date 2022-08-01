a96cfd7411464db9ec7a2c18bbf5e4ebd8e662a0
Grzegorz Milka <grzegorzmilka@gmail.com>
2022-08-01 11:59:58 +0200

    feat: implement pull-finpension

--- src/CLI.hs
+++ src/CLI.hs
@@ -2,7 +2,17 @@
 module CLI (individualPipesP) where
 
 import Bcge (pullBcge)
+import Control.Funnel (fetchTranscodeAppend)
+import Control.Monad.Cont (MonadIO)
 import Degiro (pullDegiroPortfolio)
+import FindataFetcher (
+  FindataFetcherSource (FFSourceFinpensionPortfolioTotal),
+  runFindataFetcher,
+ )
+import FindataTranscoder (
+  FindataTranscoderSource (FindataTranscoderFinpensionPortfolioTotals),
+  findataTranscoder,
+ )
 import Options.Applicative (
   Parser,
   command,
@@ -14,8 +24,25 @@ import Options.Applicative (
  )
 import Splitwise (pullSplitwise)
 import Turtle (select)
-import Turtle.Extra (textToPosixLines)
+import Turtle.Extra (posixLineToLine, textToPosixLines, textToShell)
 import Wallet (appendTransactionToWallet, getWallet)
+
+pullFinpension :: (MonadIO m) => m ()
+pullFinpension = do
+  wallet <- getWallet
+  fetchTranscodeAppend
+    (runFindataFetcher FFSourceFinpensionPortfolioTotal)
+    transcodeFinpension
+    (appendToWallet wallet)
+ where
+  transcodeFinpension =
+    findataTranscoder FindataTranscoderFinpensionPortfolioTotals
+      . fmap posixLineToLine
+      . textToShell
+  appendToWallet wallet =
+    appendTransactionToWallet wallet
+      . Turtle.select
+      . textToPosixLines
 
 pullSplitwiseFull :: IO ()
 pullSplitwiseFull = do
@@ -34,6 +61,10 @@ individualPipesP =
             "degiro-portfolio"
             "Pulls Degiro portfolio status data from Internet and appends it to the wallet."
             pullDegiroPortfolio
+        , pullCommand
+            "finpension"
+            "Pulls Finpension portfolio status data from Internet and appends it to the wallet."
+            pullFinpension
         , pullCommand
             "splitwise"
             "Pulls Splitwise data from Internet and appends Splitwise status to the wallet."
