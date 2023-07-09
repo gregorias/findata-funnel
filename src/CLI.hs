f161cd62d8f289657175000b947d12d5eac102d7
Grzegorz Milka <grzegorzmilka@gmail.com>
2023-07-09 17:55:08 +0200

    chore: release

--- src/CLI.hs
+++ src/CLI.hs
@@ -50,7 +50,7 @@ import Turtle.Prelude (rm)
 import Wallet (appendTransactionToWallet, getWallet, getWalletDir)
 
 version :: Text
-version = "2.3.0.0"
+version = "2.4.0.0"
 
 pullBcgeCc :: (MonadIO m) => m ()
 pullBcgeCc = do
